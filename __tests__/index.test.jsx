9ee0c66e98373a0a7738a624015630160e87d089
miguel bastidas <m.alejandro_80@hotmail.com>
2022-05-22 23:29:50 -0500

    implementing jest

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByRole('heading', {
+      name: 'tring again'
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
