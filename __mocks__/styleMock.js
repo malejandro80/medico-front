925a5e98f598fd601e9a63ae5bec51d14404ec42
miguel bastidas <m.alejandro_80@hotmail.com>
2022-08-01 17:14:56 -0500

    chore: fixing test

--- __mocks__/styleMock.js
+++ __mocks__/styleMock.js
@@ -1,3 +1,3 @@
 /** @format */
 
-module.exports = {}
+module.exports = {};
--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,17 +1,18 @@
+/* eslint-disable no-undef */
 /** @format */
 
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import Home from '../pages/index'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Index from '../src/pages/index.tsx';
 
 describe('Home', () => {
   it('renders a heading', () => {
-    render(<Home />)
+    render(<Index />);
 
     const heading = screen.getByRole('heading', {
-      name: 'tring again'
-    })
+      name: 'tring again',
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(heading).toBeInTheDocument();
+  });
+});
