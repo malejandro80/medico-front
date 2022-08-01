0fca881bb7f869dc1c64466da1b16df68f698d58
miguel bastidas <m.alejandro_80@hotmail.com>
2022-08-01 16:59:34 -0500

    chore: bootstraping

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,3 @@
-/** @format */
-
-import '@testing-library/jest-dom/extend-expect'
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+import '@testing-library/jest-dom/extend-expect';
