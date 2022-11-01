4e6bf2a3591fe2829447321153af520c326c98bc
CIFU1199 <edwkamilo@gmail.com>
2022-10-31 22:15:34 -0500

    Theme config whit styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import { theme } from '@/styles/Theme';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
 );
 
 export default MyApp;
